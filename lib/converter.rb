8e4216fece1b0fb0c2f97ddab254c5a6a0bc97a7
Jeremy <searls@me.com>
2016-02-12 15:00:34 -0500

    takes one table name and returns full title

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,7 +1,18 @@
 require 'json'
 require 'pry'
 
+def read_parse
 title_string = File.read('../data/temp.json')
 title_hash = JSON.parse(title_string)
+end
 
-binding.pry
+puts "Enter title:"
+input_title = gets.chomp.to_sym
+
+hash = Hash[read_parse.map{|(k,v)| [k.to_sym,v]}]
+
+if value = hash[input_title]
+  puts "#{value}"
+else
+  puts "Error, #{input_title} not found!"
+end
