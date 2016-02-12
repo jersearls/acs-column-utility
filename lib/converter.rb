f3ea54297efc24be2c28803dfdbaa844f7c6d6b3
Jeremy <searls@me.com>
2016-02-12 17:38:08 -0500

    updated JSON to reflect HIVE tables

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,20 +1,22 @@
 require 'json'
 require 'pry'
 
-def read_parse_sym
-  title_string = File.read('../data/temp.json')
+def read_parse
+  title_string = File.read('../data/names.json')
   title_hash = JSON.parse(title_string)
-  Hash[title_hash.map{|(k,v)| [k.to_sym,v]}]
 end
 
+
 def run
+  full_hash = Hash[read_parse.map{|(k,v)| [k.to_sym,v]}]
+
   puts "Enter Title or Multiple Titles (ex: title, title)"
   user_input = gets.chomp.split(", ").map(&:to_sym)
   puts
   puts "Results:"
 
   user_input.each do |title|
-    if value = read_parse_sym[title]
+    if value = full_hash[title]
       puts "#{value}"
     else
       puts
