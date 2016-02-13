c5ed1eca4a273458ba65b16a53228ec45d968ea6
Jeremy <searls@me.com>
2016-02-12 20:57:39 -0500

    formatting changed

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,23 +1,24 @@
 require 'json'
 require 'pry'
 
-def read_parse
+def read_parse_sym
   title_string = File.read('../data/names.json')
   title_hash = JSON.parse(title_string)
+  Hash[title_hash.map{|(k,v)| [k.to_sym,v]}]
 end
 
 
 def run
-  full_hash = Hash[read_parse.map{|(k,v)| [k.to_sym,v]}]
-
   puts "Enter Title or Multiple Titles (ex: title, title)"
   user_input = gets.chomp.split(", ").map(&:to_sym)
   puts
-  puts "Results:"
+  puts "RESULTS:"
+  puts
 
   user_input.each do |title|
-    if value = full_hash[title]
+    if value = read_parse_sym[title]
       puts "#{value}"
+      puts
     else
       puts
       puts "*****Error, #{title} not found!******"
