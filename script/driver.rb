e33460a7d3a17b771cc136f8d250b064bc28c70a
Jeremy <searls@me.com>
2016-02-19 23:57:49 -0500

    remove require

--- script/driver.rb
+++ script/driver.rb
@@ -1,3 +1,41 @@
-require '../lib/welcome.rb'
+require '../lib/search.rb'
+require '../lib/browse.rb'
+require '../lib/convert.rb'
+require 'pry'
+
+def home
+  puts "Welcome to the Edgewater ACS Census Assistant!"
+  puts
+  puts "Type the corresponding number to browse available columns,
+search column names for keywords or convert selected column names."
+  puts """
+  1. Browse
+  2. Search
+  3. Convert
+  4. Exit
+  """
+  print "> "
+  choice = gets.chomp.to_i
+      if choice == 1
+        puts
+        Browse.new.start_browse
+      elsif choice == 2
+        puts
+        Search.new.start_search
+      elsif choice == 3
+        puts
+        Convert.new.start_convert
+      elsif choice == 4
+        exit
+      elsif choice == 5
+        Pusheen.meow
+        home
+      else
+        puts "Invalid Entry."
+        puts
+        puts "_______________________________________________________"
+        home
+      end
+end
 
 home
