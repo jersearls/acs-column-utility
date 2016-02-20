22cdfe985e386754725da1ad7232572e7b080767
Jeremy <searls@me.com>
2016-02-19 23:56:16 -0500

    refactor browse

--- lib/browse.rb
+++ lib/browse.rb
@@ -1,37 +1,14 @@
-require '../lib/welcome.rb'
 require '../lib/modules'
 require 'pry'
 require 'json'
 
 class Browse
-  include Subject
+  include Toolbox
+
   def hash(file)
     title_string = File.read(file)
     title_hash = JSON.parse(title_string)
     Hash[title_hash.map{|(k, v)| [k.downcase, v]}]
-  end
-
-  def again
-  puts
-  puts "_______________________________________________________"
-  puts "Would you like to continue browsing? (y/n)"
-  print "> "
-  choice = gets.chomp.downcase
-    if choice == "y"
-      puts
-      puts "_______________________________________________________"
-      browse
-    elsif choice == "n"
-      puts
-      puts "_______________________________________________________"
-      home
-    elsif choice == "q" || choice == "quit" || choice == "exit"
-      exit
-    else
-      puts "That is not a valid response. Type \"y\", \"n\" or \"exit\" to quit"
-      puts
-      again
-    end
   end
 
   def title_size
@@ -51,66 +28,9 @@ class Browse
    end
   end
 
-  def pusheen
-
-    puts """
-                \"MEOW!\"
-    ───────────────────────────────────────
-    ───▐▀▄───────▄▀▌───▄▄▄▄▄▄▄─────────────
-    ───▌▒▒▀▄▄▄▄▄▀▒▒▐▄▀▀▒██▒██▒▀▀▄──────────
-    ──▐▒▒▒▒▀▒▀▒▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄────────
-    ──▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▒▒▒▒▒▒▒▒▀▄──────
-    ▀█▒▒▒█▌▒▒█▒▒▐█▒▒▒▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌─────
-    ▀▌▒▒▒▒▒▒▀▒▀▒▒▒▒▒▒▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐───▄▄
-    ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌▄█▒█
-    ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒█▀─
-    ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▀───
-    ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌────
-    ─▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐─────
-    ─▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌─────
-    ──▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐──────
-    ──▐▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▌──────
-    ────▀▄▄▀▀▀▀▀▄▄▀▀▀▀▀▀▀▄▄▀▀▀▀▀▄▄▀────────
-    """
-
-  end
-
-
-
   def start_browse
-    puts "Welcome to the browser."
-    puts "Please type the number corresponding to the subject area you'd like to view."
-
-    puts """
-    1. Race
-    2. Ancestry
-    3. Foreign Birth
-    4. Place of Birth - Native
-    5. Residence Last Year - Migration
-    6. Journey to Work
-    7. Unweighted Count
-    8. Children Relationship
-    9. Households - Families
-    10. Marital Status
-    11. Fertility
-    12. School Enrollment
-    13. Educational Attainment
-    14. Age - Sex
-    15. Language
-    16. Poverty
-    17. Disability
-    18. Income
-    19. Earnings
-    20. Veteran Status
-    21. Transfer Programs
-    22. Industry-Occupation-Class of Worker
-    23. Housing
-    24. Group Quarters
-    25. Health Insurance
-    26. Imputations
-    27. Employment Status
-    """
-  choose
+    menu("browse")
+    choose
   end
 
   def choose
@@ -136,9 +56,10 @@ class Browse
               end}
           else
             error
-            browse
+            start_browse
           end
-      again
+      try_again("browsing")
+      start_browse
   end
   error
   choose
