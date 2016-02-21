5f5344b416f0fdb7225d1124fbc0da8344cc15df
Justin Searls <searls@gmail.com>
2016-02-21 09:11:03 -0500

    Add script & lib to the load path
    This will fix the previous error so the test can require "driver"
    
    Now it blows up because `require '../search'` can't be found. That's
    because when in any directory other than driver, it doesn't line up 
    with any directories in the loadpath. We should probably use 
    require_relative for that.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,9 @@ require "minitest/pride"
 
 require 'simplecov'
 SimpleCov.start
+
+# Add script and lib to the load path
+['script', 'lib'].each do |dir|
+  $LOAD_PATH << File.join(File.dirname(__FILE__), '..', dir)
+end
+
